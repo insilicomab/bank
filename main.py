c0578138a88ceb635464e73d454a8bba884ca2ea
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2023-06-18 13:38:06 +0900

    modify

--- main.py
+++ main.py
@@ -46,11 +46,11 @@ def main(cfg: DictConfig):
         if cfg.jobs.train.run:
             now = datetime.now().strftime("%Y%m%d_%H%M%S")
             preprocess_pipeline_file_path = os.path.join(
-                cwd, f"output/{model.name}_{now}"
+                cwd, f"outputs/pipeline_{model.name}_{now}"
             )
             trainer = Trainer()
             for i, dataset in enumerate(cross_validation_datasets):
-                save_file_path = os.path.join(cwd, f"output/{model.name}_{now}_{i}")
+                save_file_path = os.path.join(cwd, f"outputs/{model.name}_{now}_{i}")
 
                 x_train, x_valid, y_train, y_valid = dataset
                 evaluation, artifact = trainer.train_and_evaluate(
