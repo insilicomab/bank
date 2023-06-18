2e0957adcfdb687e67c7a51378aeea97ec2fba07
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2023-06-18 12:32:21 +0900

    update

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import os
+from datetime import date, datetime, timedelta
 
 import hydra
 import mlflow
 from omegaconf import DictConfig
 
 from src.jobs.retrieve import DataRetriever
+from src.jobs.train import Trainer
 from src.middleware.logger import configure_logger
+from src.models.models import MODELS
 from src.models.preprocess import DataPreprocessPipeline
 
 logger = configure_logger(__name__)
@@ -27,9 +30,41 @@ def main(cfg: DictConfig):
             file_path=cfg.train_file_path,
         )
         data_preprocess_pipeline = DataPreprocessPipeline()
-        cross_validation_dataset = data_retriever.stratified_kfold_split(
+        cross_validation_datasets = data_retriever.stratified_kfold_split(
             raw_df, data_preprocess_pipeline
         )
+        _model = MODELS.get_model(name=cfg.jobs.model.name)
+        model = _model.model(
+            early_stopping_rounds=cfg.jobs.model.params.early_stopping_rounds,
+            eval_metrics=cfg.jobs.model.params.eval_metrics,
+            verbose_eval=cfg.jobs.model.params.verbose_eval,
+        )
+
+        if "params" in cfg.jobs.model.keys():
+            model.reset_model(params=cfg.jobs.model.params)
+        if cfg.jobs.train.run:
+            now = datetime.now().strftime("%Y%m%d_%H%M%S")
+            preprocess_pipeline_file_path = os.path.join(cwd, f"{model.name}_{now}")
+            trainer = Trainer()
+            for i, dataset in enumerate(cross_validation_datasets):
+                save_file_path = os.path.join(cwd, f"{model.name}_{now}_{i}")
+
+                x_train, x_valid, y_train, y_valid = dataset
+                evaluation, artifact = trainer.train_and_evaluate(
+                    model=model,
+                    x_train=x_train,
+                    y_train=y_train,
+                    x_test=x_valid,
+                    y_test=y_valid,
+                    data_preprocess_pipeline=data_preprocess_pipeline,
+                    preprocess_pipeline_file_path=preprocess_pipeline_file_path,
+                    save_file_path=save_file_path,
+                )
+                mlflow.log_metric(
+                    "area_under_the_curve", evaluation.area_under_the_curve
+                )
+                mlflow.log_artifact(artifact.preprocess_file_path, "preprocess")
+                mlflow.log_artifact(artifact.model_file_path, "model")
 
 
 if __name__ == "__main__":
