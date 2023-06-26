b2cfb888ca677ae53a20af752a33717c89dffe78
insilicomab <nakada.mitsutaka.c76@kyoto-u.jp>
2023-06-26 14:52:53 +0900

    update

--- ml/src/middleware/logger.py
+++ ml/src/middleware/logger.py
@@ -6,4 +6,5 @@ LOG_LEVEL = os.getenv("LOG_LEVEL", logging.DEBUG)
 
 def configure_logger(name) -> logging.Logger:
     logger = logging.getLogger(name)
+    logger.setLevel(LOG_LEVEL)
     return logger
